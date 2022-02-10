90d927b54a3b4c31ba23af5b2f2711498423c9d1
zl0i <dmitri.popov.96@mail.ru>
2022-02-10 19:58:18 +0900

    refractor: header as component

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
-    <div class="header">
-      <nav>
-        <a href="#">
-          <h1>_zloi_</h1>
-        </a>
-        <div class="header-links">
-          <a href="#summary">{{ $t("headers.summary") }}</a>
-          <a href="#knowledge">{{ $t("headers.knoweledge") }}</a>
-          <a href="#reads">{{ $t("headers.reads") }}</a>
-          <div class="language">
-            <span>Translate</span>
-            <img alt="arrow" src="./assets/arrow-down.svg" />
-            <div class="language-background">
-              <div class="language-popup">
-                <span @click="setLanguage('ru')">Русский</span>
-                <span @click="setLanguage('en')">English</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <Header @changeLanguage="setLanguage"></Header>
     <Welcome :text="about" :titles="titles" :links="links" />
     <Summary id="summary" :summary="summary" />
     <div class="gallery-box">
@@ -35,6 +14,7 @@
 import { Options, Vue } from "vue-class-component";
 import axios from "axios";
 import Welcome, { ILink } from "./components/Welcome.vue";
+import Header from "./components/Header.vue";
 import Summary from "./components/Summary.vue";
 import KnoweledgeView from "./components/Knowledge.vue";
 import BookView from "./components/BooksView.vue";
@@ -42,6 +22,7 @@ import { i18n } from "./i18n";
 
 @Options({
   components: {
+    Header,
     Welcome,
     Summary,
     KnoweledgeView,
@@ -121,102 +102,4 @@ body::-webkit-scrollbar-thumb {
 }
 </style>
 
-<style scoped>
-.gallery-box {
-  min-height: 100vh;
-}
-.header {
-  padding: 0 8.5%;
-}
 
-.header nav {
-  text-align: right;
-}
-
-.header-links {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  flex-grow: 1;
-  font-size: 24px;
-  letter-spacing: 1px;
-}
-@media (max-device-width: 736px) {
-  .header-links {
-    display: none;
-  }
-  .header nav a h1 {
-    margin-bottom: 0;
-  }
-}
-
-.header-links > * {
-  margin-left: 40px;
-  color: #ffffff;
-}
-
-.header-links a:hover,
-span:hover {
-  color: #94aadd;
-}
-
-.header-links * {
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-
-.language {
-  width: fit-content;
-  margin-left: 100px;
-  cursor: pointer;
-}
-
-.language img {
-  margin-left: 5px;
-}
-
-.language-background {
-  display: none;
-  position: absolute;
-  padding-top: 10px;
-}
-
-.language:hover .language-background {
-  display: block;
-}
-
-.language-popup {
-  background: #1c1e2a;
-  border: 1px solid rgba(255, 255, 255, 0.5);
-  box-sizing: border-box;
-  box-shadow: 0 0 6px rgba(255, 255, 255, 0.75);
-  border-radius: 2px;
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-  align-items: flex-start;
-  padding: 0.6rem 1.5rem 0.6rem 1.25rem;
-}
-
-nav {
-  display: flex;
-  width: 100%;
-  align-items: center;
-  flex-wrap: wrap;
-  padding: 0px 0 10px;
-}
-
-nav a {
-  text-decoration: none;
-}
-
-nav a h1 {
-  font-family: "Permanent Marker";
-  color: #fff;
-  font-size: 40px;
-}
-</style>
---
+++ src/src/components/Header.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="header">
+    <nav>
+      <a href="#">
+        <h1>_zloi_</h1>
+      </a>
+      <div class="header-links">
+        <a href="#summary">{{ $t("headers.summary") }}</a>
+        <a href="#knowledge">{{ $t("headers.knoweledge") }}</a>
+        <a href="#reads">{{ $t("headers.reads") }}</a>
+        <div class="language">
+          <span>Translate</span>
+          <img alt="arrow" src="../assets/arrow-down.svg" />
+          <div class="language-background">
+            <div class="language-popup">
+              <span @click="changeLanguage('ru')">Русский</span>
+              <span @click="changeLanguage('en')">English</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { Emit } from "vue-property-decorator";
+import { i18n } from "../i18n";
+
+@Options({})
+export default class Header extends Vue {
+  @Emit("changeLanguage")
+  changeLanguage(locale: typeof i18n.global.locale) {
+    this.$emit("changeLanguage", locale);
+  }
+}
+</script>
+
+
+<style scoped>
+.gallery-box {
+  min-height: 100vh;
+}
+.header {
+  padding: 0 8.5%;
+}
+
+.header nav {
+  text-align: right;
+}
+
+.header-links {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  flex-grow: 1;
+  font-size: 24px;
+  letter-spacing: 1px;
+}
+@media (max-device-width: 736px) {
+  .header-links {
+    display: none;
+  }
+  .header nav a h1 {
+    margin-bottom: 0;
+  }
+}
+
+.header-links > * {
+  margin-left: 40px;
+  color: #ffffff;
+}
+
+.header-links a:hover,
+span:hover {
+  color: #94aadd;
+}
+
+.header-links * {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.language {
+  width: fit-content;
+  margin-left: 100px;
+  cursor: pointer;
+}
+
+.language img {
+  margin-left: 5px;
+}
+
+.language-background {
+  display: none;
+  position: absolute;
+  padding-top: 10px;
+}
+
+.language:hover .language-background {
+  display: block;
+}
+
+.language-popup {
+  background: #1c1e2a;
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  box-sizing: border-box;
+  box-shadow: 0 0 6px rgba(255, 255, 255, 0.75);
+  border-radius: 2px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  align-items: flex-start;
+  padding: 0.6rem 1.5rem 0.6rem 1.25rem;
+}
+
+nav {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 0px 0 10px;
+}
+
+nav a {
+  text-decoration: none;
+}
+
+nav a h1 {
+  font-family: "Permanent Marker";
+  color: #fff;
+  font-size: 40px;
+}
+</style>
