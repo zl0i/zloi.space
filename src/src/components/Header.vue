1949abe46479e5806b3d0d8fea1675ebf6c6d406
GoSSy <vitea1994@gmail.com>
2022-03-27 17:14:26 +0300

    feat: mobile header menu

--- src/src/components/Header.vue
+++ src/src/components/Header.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="header">
+    <img alt="menu" class="burger-button" src="../../public/img/menu-sharp.svg" @click="showMenu(!isOpenMenu)">
+    <div class="mobile-view" v-if="isOpenMenu">
+      <div class="mobile-menu-blur" @click="showMenu(false)"></div>
+      <div class="mobile-menu">
+        <p class="mobile-header">_zloi_</p>
+        <a href="#summary" @click="showMenu(false)">{{ $t("headers.summary") }}</a>
+        <a href="#knowledge" @click="showMenu(false)">{{ $t("headers.knoweledge") }}</a>
+        <a href="#reads" @click="showMenu(false)">{{ $t("headers.reads") }}</a>
+        <a>{{ $t("headers.languages") }}:</a>
+        <span @click="changeLanguage('ru')">Русский</span>
+        <span @click="changeLanguage('en')">English</span>
+      </div>
+    </div>
     <nav>
       <a href="#">
         <h1>_zloi_</h1>
@@ -29,9 +42,22 @@ import { i18n } from "../libs/i18n";
 
 @Options({})
 export default class Header extends Vue {
+  isOpenMenu: boolean = false
+  showMenu(isOpenMenu: boolean) {
+    if (this.isOpenMenu) {
+      document.body.style.overflow = "auto";
+      document.body.style.position = "static";
+      this.isOpenMenu = false;
+    } else {
+      document.body.style.overflow = "hidden";
+      document.body.style.position = "fixed";
+      this.isOpenMenu = true;
+    }
+  }
   @Emit("changeLanguage")
   changeLanguage(locale: typeof i18n.global.locale) {
     this.$emit("changeLanguage", locale);
+    this.isOpenMenu=false
   }
 }
 </script>
@@ -40,10 +66,15 @@ export default class Header extends Vue {
 <style scoped>
 .header {
   padding: 0 8.5%;
+  display: flex;
 }
 
 .header nav {
   text-align: right;
+}
+
+.burger-button {
+  display: none;
 }
 
 .header-links {
@@ -54,12 +85,73 @@ export default class Header extends Vue {
   font-size: 24px;
   letter-spacing: 1px;
 }
+
+.mobile-view {
+  display: none;
+}
+
+.mobile-header {
+  font-size: 24px;
+  font-family: 'Permanent Marker';
+}
+
 @media (max-device-width: 736px) {
-  .header-links {
-    display: none;
+  .header {
+    margin-top: 15px;
   }
   .header nav a h1 {
-    margin-bottom: 0;
+    margin: 0 0 0 25px;
+  }
+  .burger-button {
+    display: block;
+    width: 45px;
+  }
+  .mobile-view {
+    display: block;
+    position: fixed;
+    top: 0; right: 0; bottom: 0; left: 0;
+    overflow: hidden;
+    z-index: 10;
+  }
+  .mobile-menu-blur {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.66);
+    backdrop-filter: blur(4px);
+  }
+  .mobile-menu {
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    top: 0;
+    left: 0;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: 25px;
+    padding: 19px 0 0 30px;
+    background: #292D48;
+    width: 55%;
+    height: 100%;
+    z-index: 10;
+  }
+  .mobile-menu a, .mobile-menu span {
+    color: #ffffff;
+    font-family: 'Roboto';
+    font-size: 20px;
+    text-decoration: none;
+  }
+  .mobile-menu a, .mobile-menu span {
+    margin: 0;
+  }
+  .mobile-menu span {
+    margin-left: 25px;
+  }
+  .header-links {
+    display: none;
   }
 }
 
