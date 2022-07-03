6cd8c37316f2bea3f0e5d51bf2f5601467cd585d
zl0i <dmitri.popov.96@mail.ru>
2022-07-03 05:54:23 +0300

    feat(api): images

--- api/routes/summary.router.ts
+++ api/routes/summary.router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { UploadedFile } from 'express-fileupload'
 import { SummaryService } from '../services/summary.service'
 
 const router = Router()
@@ -36,37 +37,55 @@ router.get('/about', async (req, res) => {
   }
 })
 
-// router.post('/:key', async (req, res) => {
-//   try {
-//     const key = req.params.key
-//     const { position, language, value } = req.body
-//     const status = await SummaryService.setKey(key, position, language, value)
-//     res.json({ status: status })
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).end("internal error")
-//   }
-// })
-
-// router.patch('/:key', async (req, res) => {
-//   try {
-//     const key = req.params.key
-//     console.log(key)
-//     const { position, language, value } = req.body
-//     const status = await SummaryService.updateKey(key, position, language, value)
-//     res.json({ status: status })
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).end("internal error")
-//   }
-// })
-
 router.post('/about', async (req, res) => {
   try {
     const { lang, titles, about } = req.body
     console.log(req.body)
     const status = await SummaryService.updateAbout(lang, titles, about)
     res.json({ status })
+  } catch (error) {
+    console.log(error)
+    res.status(500).end("internal error")
+  }
+})
+
+router.get('/links', async (_req, res) => {
+  try {
+    const data = await SummaryService.getLinks()
+    res.json(data)
+  } catch (error) {
+    console.log(error)
+    res.status(500).end("internal error")
+  }
+})
+
+router.get('/links/image/:name', async (req, res) => {
+  try {
+    const data = await SummaryService.getImageLink(req.params.name)
+    res.setHeader('Content-Type', `image/${data.type}`)
+    res.end(data.blob)
+  } catch (error) {
+    console.log(error)
+    res.status(500).end("internal error")
+  }
+})
+
+router.post('/links/:name', async (req, res) => {
+  try {
+    const file = req.files?.['icon'] as UploadedFile
+    const data = await SummaryService.createLink(req.params.name, req.body.link, file.data, file.name)
+    res.json(data)
+  } catch (error) {
+    console.log(error)
+    res.status(500).end("internal error")
+  }
+})
+
+
+router.delete('/links/:name', async (req, res) => {
+  try {
+    const data = await SummaryService.deleteLink(req.params.name)
+    res.json(data)
   } catch (error) {
     console.log(error)
     res.status(500).end("internal error")
--- api/services/summary.service.ts
+++ api/services/summary.service.ts
@@ -1,3 +1,4 @@
+import { Links } from '../entity/links.entity'
 import { Summary } from '../entity/summary.entity'
 import { AppDataSource } from '../src/db'
 
@@ -67,6 +68,43 @@ export class SummaryService {
             await Summary.insert({ language: lang, key: 'about', value: about })
         }
         return true
+    }
+
+    static async getLinks() {
+        const links = await Links.find({ select: ['id', 'name', 'link'] })
+        return links
+    }
+
+    static async getImageLink(name: string) {
+        try {
+            const link = await Links.findOne({ where: { name } })
+            return {
+                type: link?.type,
+                blob: Buffer.from(link?.icon as string, 'base64')
+            }
+        } catch (error) {
+            throw new Error('domain error')
+        }
+
+    }
+
+    static async createLink(name: string, link: string, data: Buffer, iconname: string) {
+        try {
+            const type: string = iconname.split('.').pop() as string
+            await Links.upsert({ name, link, type, icon: data.toString('base64') }, { conflictPaths: ['name'] })
+            return { status: 'ok' }
+        } catch (error) {
+            throw new Error('domain error')
+        }
+    }
+
+    static async deleteLink(name: string) {
+        try {
+            await Links.delete({ name })
+            return { status: 'ok' }
+        } catch (error) {
+            throw new Error('domain error')
+        }
     }
 
 }
