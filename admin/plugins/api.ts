950a2ec2df7ab2d0e1e65e94900c9d65b9016526
zl0i <dmitri.popov.96@mail.ru>
2022-07-06 17:06:23 +0300

    refactor(admin): plugin api

---
+++ admin/plugins/api.ts
@@ -0,0 +1,114 @@
+import type { Context, Plugin } from '@nuxt/types'
+
+
+interface API {
+    pullAbout: (lang: string) => Promise<any>
+    pushAbout: (lang: string, titles: string[], about: string) => Promise<any>
+    pullLinks: () => Promise<any>
+    pushLink: (name: string, link: string, blob: string) => Promise<any>
+    patchLink: (id: number, name: string, link: string, blob: string) => Promise<any>
+    deleteLink: (id: number) => Promise<any>
+}
+
+declare module 'vue/types/vue' {
+    interface Vue {
+        $api: API
+    }
+}
+
+declare module '@nuxt/types' {
+    interface Context {
+        $api: API
+    }
+    interface Context {
+        $api: API
+    }
+}
+
+declare module 'vuex/types/index' {
+    interface Store<S> {
+        $api: API
+    }
+}
+
+function fromContext({ $axios, store }: Context) {
+    const api: API = {
+        async pullAbout(lang: string) {
+            const { data } = await $axios.get("/summary/about", {
+                params: {
+                    language: lang,
+                },
+            });
+            return data
+        },
+        async pushAbout(lang: string, titles: string[], about: string) {
+            const { data } = await $axios.post(
+                "/summary/about",
+                {
+                    lang: lang,
+                    titles: titles,
+                    about: about,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${store.state["adminKey"]}`,
+                    },
+                }
+            );
+            return data
+        },
+        async pullLinks() {
+            const { data } = await $axios.get("/links");
+            return data
+        },
+        async pushLink(name: string, link: string, blob: string) {
+            const { data } = await $axios.post(
+                `/links/`,
+                {
+                    name: name,
+                    blob: blob,
+                    link: link,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${store.state["adminKey"]}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            return data
+        },
+        async patchLink(id: number, name: string, link: string, blob: string) {
+            const { data } = await $axios.patch(
+                `/links/${id}`,
+                {
+                    name: name,
+                    blob: blob,
+                    link: link,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${store.state["adminKey"]}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            return data
+        },
+        async deleteLink(id: number) {
+            await $axios.delete(`/links/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${store.state["adminKey"]}`,
+                },
+            });
+        }
+    }
+    return api
+}
+
+
+const apiPlugin: Plugin = (context, inject) => {
+    inject('api', fromContext(context))
+}
+
+export default apiPlugin
