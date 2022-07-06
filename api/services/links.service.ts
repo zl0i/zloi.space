203870b63fd99f3048cd60589a94e6a04cfb3577
zl0i <dmitri.popov.96@mail.ru>
2022-07-06 06:59:09 +0300

    refactor(api): links

--- api/services/links.service.ts
+++ api/services/links.service.ts
@@ -2,15 +2,15 @@ import { Links } from '../entity/links.entity'
 
 export class LinksService {
 
-    static async getLinks() {
+    static async get() {
         const links = await Links.find({ select: ['id', 'name', 'link', 'blob'] })
-        for(const link of links) {
+        for (const link of links) {
             link.blob = link.blob.toString()
         }
         return links
     }
 
-    static async getImageLink(name: string) {
+    static async getImage(name: string) {
         try {
             const link = await Links.findOne({ where: { name } })
             if (!link) {
@@ -29,9 +29,19 @@ export class LinksService {
 
     }
 
-    static async createLink(name: string, link: string, blob: string) {
+    static async create(name: string, url: string, blob: string) {
         try {
-            await Links.upsert({ name, link, blob }, { conflictPaths: ['name'] })
+            const link = await Links.insert({ name, link: url, blob })
+            return { status: 'ok', id: link.raw[0].id }
+        } catch (error) {
+            console.log(error)
+            throw new Error('domain error')
+        }
+    }
+
+    static async update(id: number, name: string, link: string, blob: string) {
+        try {
+            await Links.update({ id: id }, { name, link, blob })
             return { status: 'ok' }
         } catch (error) {
             console.log(error)
@@ -39,9 +49,9 @@ export class LinksService {
         }
     }
 
-    static async deleteLink(name: string) {
+    static async delete(id: number) {
         try {
-            await Links.delete({ name })
+            await Links.delete({ id })
             return { status: 'ok' }
         } catch (error) {
             throw new Error('domain error')
