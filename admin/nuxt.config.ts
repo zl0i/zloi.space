d3cf805db700bd6b7a8bd4889394859237c487e9
zl0i <dmitri.popov.96@mail.ru>
2022-08-24 13:36:18 +0300

    chore: docker-compose

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -36,7 +36,7 @@ const config: NuxtConfig = {
     https: isDev ? false : true,
   },
   server: {
-    host: isDev ? "localhost" : "0.0.0.0",
+    host: "0.0.0.0",
     port: isDev ? 8081 : 3000
   },
   build: {
