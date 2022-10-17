541aa889ad39f6075d28c3dc0488b7545af6b38f
zl0i <dmitri.popov.96@mail.ru>
2022-10-18 07:20:29 +0900

    feat: github webhook

--- api/routes/instructions.router.ts
+++ api/routes/instructions.router.ts
@@ -1,6 +1,10 @@
+import Crypto from 'crypto'
 import express, { Router } from 'express'
 import { auth } from '../middleware/auth'
 import { InstructionsService } from '../services/instruction.service'
+
+
+const GITHUB_SECRET_WEBHOOK = process.env['GITHUB_SECRET_WEBHOOK'] ?? ''
 
 const router = Router()
 
@@ -18,6 +22,28 @@ router.post('/', [auth()], async (_req: express.Request, res: express.Response) 
   try {
     const inst = await InstructionsService.update()
     res.json(inst)
+  } catch (error) {
+    console.log(error)
+    res.status(500).end("internal error")
+  }
+})
+
+router.post('/github', async (req: express.Request, res: express.Response) => {
+  try {
+    const hmac = Crypto.createHmac('sha256', GITHUB_SECRET_WEBHOOK)
+    hmac.update(JSON.stringify(req.body));
+    const calculatedSignature = 'sha256=' + hmac.digest('hex');
+    
+    console.log(req.body)
+    console.log(calculatedSignature, req.headers['x-hub-signature-256'])
+
+    if (req.headers['x-hub-signature-256'] === calculatedSignature) {
+      console.log('all good');
+      await InstructionsService.update()
+    } else {
+      console.log('not good');
+    }
+    res.end()
   } catch (error) {
     console.log(error)
     res.status(500).end("internal error")
